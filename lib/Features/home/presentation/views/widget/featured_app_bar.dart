58bdadd51c5f3dd591d31f9f7d18e6a4196b5532
Abdalrhmanabood <abdalrhmanabood16@gmail.com>
2023-06-03 21:42:18 +0200

    create styles file and Add title to home view

--- lib/Features/home/presentation/views/widget/featured_app_bar.dart
+++ lib/Features/home/presentation/views/widget/featured_app_bar.dart
@@ -8,7 +8,7 @@ class FeaturedAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 40,left: 24 , right: 24 , bottom: 20),
+      padding: const EdgeInsets.only(top: 40 ,left: 12, bottom: 20),
       child: Row(
         children: [
           Image.asset(AssetsData.logo, height: 18),
--- lib/Features/home/presentation/views/widget/home_view_body.dart
+++ lib/Features/home/presentation/views/widget/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'featured_app_bar.dart';
 import 'featured_book_list_view.dart';
@@ -7,11 +8,25 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        FeaturedAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return const Padding(
+      padding:  EdgeInsets.symmetric(horizontal:24),
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          FeaturedAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 12),
+            child: Text(
+              'Best Seller',
+              style: Styles.titleMedium,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
