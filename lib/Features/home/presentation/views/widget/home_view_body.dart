1abc87bfe7195f7957a3a63feba368e9668e4921
Abdalrhmanabood <abdalrhmanabood16@gmail.com>
2023-06-05 14:47:02 +0200

    Best seller item part 2

--- lib/Features/home/presentation/views/widget/home_view_body.dart
+++ lib/Features/home/presentation/views/widget/home_view_body.dart
@@ -10,7 +10,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
+      padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -22,6 +22,9 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
           ),
           BestSellerListViewItem(),
         ],
@@ -42,7 +45,6 @@ class BestSellerListViewItem extends StatelessWidget {
           AspectRatio(
             aspectRatio: 2.5 / 4,
             child: Container(
-              
               decoration: BoxDecoration(
                 color: Colors.red,
                 borderRadius: BorderRadius.circular(16),
@@ -52,6 +54,23 @@ class BestSellerListViewItem extends StatelessWidget {
                 ),
               ),
             ),
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children:  [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: const Text(
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.textStyle20,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  
+                ),
+              ),
+            ],
           )
         ],
       ),
